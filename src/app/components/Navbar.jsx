d49e33daa8009ce995c8aa5397fe66b89361baae
istiakiraz <istiakiraz@gmail.com>
2025-07-26 03:23:27 +0600

    first commit

---
+++ src/app/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div><nav className='flex justify-center gap-20 bg-gray-700 py-5'>
+          <ul>
+            Home
+          </ul>
+          <ul>
+            Services
+          </ul>
+          <ul>
+            About
+          </ul>
+        </nav></div>
+  )
+}
